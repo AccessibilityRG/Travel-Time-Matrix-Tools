2ac9c10b2ebed7faff3b3893addd5f625d9caa74
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2014-08-19 09:22:06 +0300

    Cleaning code.

--- SelectFiles_tools.py
+++ SelectFiles_tools.py
@@ -3,15 +3,16 @@ __author__ = 'hentenka'
 import os, sys, random, shutil
 
 def listFiles(topPath):
+    ''' Creates a list from Travel Time Matrix file paths that are found from the directory and sub-folders of "topPath" '''
     f = []
     for root, dirs, files in os.walk(topPath):
         for filename in files:
             if filename.startswith('time_to_'):
                 f.append(os.path.join(root, filename))
-
     return f
 
 def selectQuery(inputFilesList,inputIDs):
+    ''' Searches files based on inputIDs (YKR-ID) from the "inputFilesList" '''
     selected = []
     for id in inputIDs:
         q = 'time_to_' + str(id) + '.txt'
@@ -23,22 +24,36 @@ def selectQuery(inputFilesList,inputIDs):
     return selected
 
 def selectRandom(inputList, sampleSize):
+    '''Creates a random selection of files with chosen sample size'''
     return random.sample(inputList, sampleSize)
 
 def copyFiles(inputFilesList, destinationFolder):
+    '''Copies files to a chosen destination folder'''
+
+    #If folder does not exist, create one.
+    if not os.path.isdir(destinationFolder):
+        os.mkdir(destinationFolder)
+
     for file in inputFilesList:
         shutil.copy2(file, destinationFolder)
 
 def main():
-    path = r"C:\HY-Data\HENTENKA\Python\MassaAjoNiputus\MetropAccess-matka-aikamatriisi_Ajot_2014_04\MetropAccess-matka-aikamatriisi_TOTAL_FixedInternalCells"
-    sampleFolder = r"C:\HY-Data\HENTENKA\Python\MassaAjoNiputus\MetropAccess-matka-aikamatriisi_Ajot_2014_04\QualityCheck"
-    files = listFiles(path)
+    inputPath = r"...\MetropAccess-matka-aikamatriisi_TOTAL"
+    outputFolder = r"...\Test"
 
+    #List all Matrix files
+    files = listFiles(inputPath)
+
+    #Create random selection with chosen sample size
     #randFiles = selectRandom(files, 600)
-    selectedFiles = selectQuery(files, [5894644,5970404])
 
-    print selectedFiles
-    #copyFiles(randFiles, sampleFolder)
+    #Select files based on list of YKR-IDs
+    YKR_ids = [5894644,5970404]
+    selectedFiles = selectQuery(files, YKR_ids)
+
+    #Copy files to a directory
+    copyFiles(selectedFiles, outputFolder)
+    #copyFiles(randFiles, outputFolder)
 
 if __name__ == '__main__':
     main()
